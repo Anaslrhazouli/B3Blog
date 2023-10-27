4524500f4850ea13a9fc8f58a9c5a04bc6798f7a
Anaslrhazouli <anaslrh2@outlook.com>
2023-10-27 08:35:45 +0200

    commentaire + profile

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+        
         return $this->render('accueil/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
             'controller_name' => 'AccueilController',
+            
         ]);
     }
 }
